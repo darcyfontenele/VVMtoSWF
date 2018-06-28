33ac608bdd8a543891fee2855ab68fe77dc4c02b
darcyfontenele <darcyfontenele@gmail.com>
2018-06-28 16:57:07 -0300

    SWF file create

--- main.py
+++ main.py
@@ -1,33 +1,27 @@
 import csv
 
 # list_vvm_files = ['snap01.csv','snap02.csv','snap03.csv','snap04.csv','snap05.csv','snap06.csv','snap07.csv']
-list_vvm_files = ['snap01.csv']
+list_csv_files = ['snap01.csv']
+list_instance = []
 
+print("Reading VisualVM files...")
 
-print("Reading VisaulVM files...")
-
-vvm_class = {}
-vvm_total = 0
-
-for vvm_file in list_vvm_files:
+for vvm_file in list_csv_files:
     print("Reading VisualVM file: ",vvm_file)
     with open(vvm_file, newline='') as csvfile:
         reader = csv.DictReader(csvfile, delimiter=',')
+        list_instance_tmp = None
         for line in reader:
-            print(line["Class Name - Live Objects"]),
-            print(line["Live Bytes [%]"]),
-            print(line["Live Bytes"]),
-            print(line["Live Objects"])
-            if line["Class Name - Live Objects"].startswith('br.org.eldorado.ead2pcd.'):
-                (a,b,class_name) = line["Class Name - Live Objects"].rpartition('.')
-                (class_name,a,b) = class_name.rpartition('$$EnhancerByCGLIB$$')
-                #class_name.rpartition('$$EnhancerByCGLIB$$')[0]
-                if class_name.endswith('[]'):
-                   class_name =class_name[:-2]
-                if class_name in vvm_class:
-                    vvm_class[class_name] += line["Live Objects"]
-                    vvm_total += float(line["Live Objects"])
-                else:
-                    vvm_class[class_name] = line["Live Objects"]
-                    vvm_total += float(line["Live Objects"])
+            # print(line["Class Name - Live Objects"]),
+            # print(line["Live Bytes [%]"]),
+            # print(line["Live Bytes"]),
+            # print(line["Live Objects"])
+            list_instance_tmp = [line["Class Name - Live Objects"], line["Live Bytes [%]"],
+                                 line["Live Bytes"], line["Live Objects"]]
+            list_instance.append(list_instance_tmp)
         csvfile.close()
+
+for instance in list_instance:
+    print(instance)
+
+f = open("swttest.swf", "w+")
